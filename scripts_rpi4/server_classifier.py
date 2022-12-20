8cbbe836fd30f3c267d889f969053d2703bffd0f
Ken Yoshizoe <kenyoshizoe@gmail.com>
2022-12-20 16:43:16 +0900

    fix server_classifier

--- scripts_rpi4/server_classifier.py
+++ scripts_rpi4/server_classifier.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3
 import sys
 import yaml
 import pickle
@@ -12,28 +12,6 @@ from util.regression import load_model, predict
 from util.qrcode import detect_roi
 from evaluation import evaluation
 
-###### EDIT HERE #################################
-
-
-def classify_roi(net, model, code, rect, roi, roi_rect):
-    if (roi is None):
-        return None
-
-    x = net([roi])
-    x = predict(model, x)
-    x = float(x[0])
-    return x
-
-    '''
-    # Simple TTA
-    mirror = np.fliplr( roi ).copy()
-    x = net([roi,mirror])
-    x = predict( model, x )
-    return float(x.mean())
-    '''
-
-##################################################
-
 
 class Classifier:
     def __init__(self, params, size_ratio=None, gap_ratio=None, th_area=None):
@@ -45,9 +23,10 @@ class Classifier:
 
         # setup network
         self.net = CNN(params['network'])
+
         # load model
-        model_name = params['name'] + '_' + params['network'] + '.pkl'
-        self.model = load_model(model_name)
+        filename = params['name'] + '_' + params['network'] + '.pkl'
+        self.model = pickle.load(open('model/' + filename, 'rb'))
 
     def __call__(self, img):
         (code, rect), (roi, roi_rect) = detect_roi(
@@ -57,27 +36,22 @@ class Classifier:
             'code': code,
             'rect': rect,
             'roi_rect': roi_rect,
-            'pred': classify_roi(self.net, self.model, code, rect, roi, roi_rect),
+            'pred': evaluation(self.model, self.net, roi, code, self.params['evaluation']),
         }
 
         return data
 
 
-def main(params):
+def main():
     # load params
     params = None
     with open('config/default.yaml') as f:
         params = yaml.safe_load(f)
-    # load model
-    filename = params['name'] + '_' + params['network'] + '.pkl'
-    model = pickle.load(open('model/' + filename, 'rb'))
+
     # steup network
-    net = CNN(params['network'])
-
-    classifier = Classifier()
-
-    server_start(5555, dnn, verbose=verbose, imfile=imfile,
-                 imshow=imshow, vs_str=vs_str, zmq_mode=zmq_mode)
+    classifier = Classifier(params)
+    server_start(5555, classifier, verbose=False, imfile='server.jpg',
+                 imshow=params['server']['preview'], vs_str=['ant', 'bee'], zmq_mode=3)
 
 
 # python server_classifier.py -v --port 5555
